d1b66a808d51f548de403f438a1f819072015a3f
NS977255 <112407729+NS977255@users.noreply.github.com>
2025-04-12 12:13:10 -0400

    fishing area collision

---
+++ cosmic-fishing/scenes/fish_area.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+var rand = RandomNumberGenerator.new()
+
+func _ready():
+	rand.randomize()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("bobber"):
+		var bobtime = rand.randi_range(10, 30)
+		$FishTimer.wait_time = bobtime
+		$FishTimer.start()
+
+
+func _on_fish_timer_timeout() -> void:
+	#Give Haptic feedback to controller.
+	#Call some function on player that awaits a collision
+	pass
