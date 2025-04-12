77861c433894f6a4c429e22578d01ed77439be47
NA980593 <90323608+NA980593@users.noreply.github.com>
2025-04-12 15:20:46 -0400

    add beer effects

---
+++ cosmic-fishing/scripts/beer.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+@onready var beer_particles: CPUParticles3D = $CPUParticles3D
+
+func _physics_process(delta: float) -> void:
+	if abs(rad_to_deg(rotation.x)) > 35 or abs(rad_to_deg(rotation.y)) > 35 or abs(rad_to_deg(rotation.z)) > 35:
+		beer_particles.emitting = true
+	else:
+		beer_particles.emitting = false
