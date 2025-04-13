32760511ea5aa0e61ee3cee326414a90d360b036
NA980593 <90323608+NA980593@users.noreply.github.com>
2025-04-13 06:18:30 -0400

    test

--- cosmic-fishing/scripts/fish_spawner.gd
+++ cosmic-fishing/scripts/fish_spawner.gd
@@ -1,8 +1,5 @@
 extends Node3D
 
-func _ready() -> void:
-	spawn("res://assets/3d_models/good-fish/Anglerfish.glb")
-
 func spawn(name):
 	var scene = load(name)
 	var instance = scene.instantiate()
