6f2971377552cdc530e53ab1257a8d937504719a
NA980593 <90323608+NA980593@users.noreply.github.com>
2025-04-13 02:57:46 -0400

    add rod_caster catching bits

--- cosmic-fishing/scripts/rod_caster.gd
+++ cosmic-fishing/scripts/rod_caster.gd
@@ -4,11 +4,22 @@ extends Node3D
 
 @onready var ray_cast_mesh: MeshInstance3D = $RayCastMesh
 
+@onready var timer: Timer = $Timer
+
+signal catch_fish
+
 func _ready() -> void:
 	ray_cast_mesh.visible = false
 
 func _process(delta: float) -> void:
 	if ray_cast.is_colliding() && ray_cast_mesh.visible == false:
 		ray_cast_mesh.visible = true
+		timer.start()
 	if !ray_cast.is_colliding() && ray_cast_mesh.visible == true:
 		ray_cast_mesh.visible = false
+		timer.stop()
+
+
+func _on_timer_timeout() -> void:
+	emit_signal("catch_fish")
+	get_tree().reload_current_scene()
