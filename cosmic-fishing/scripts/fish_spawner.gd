f64922e6396122e44ac145243ae67625b5a76050
NA980593 <90323608+NA980593@users.noreply.github.com>
2025-04-13 08:39:53 -0400

    eta

--- cosmic-fishing/scripts/fish_spawner.gd
+++ cosmic-fishing/scripts/fish_spawner.gd
@@ -2,6 +2,6 @@ extends Node3D
 
 func spawn(name):
 	print(name)
-	var scene = load(OS.get_executable_path().get_base_dir().path_join("/scenes/good_fish_scenes/"+name))
+	var scene = load("res://scenes/good_fish_scenes/"+name)
 	var instance = scene.instantiate()
 	add_child(instance)
