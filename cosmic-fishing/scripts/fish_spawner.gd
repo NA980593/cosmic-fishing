a0a9e4b950e64c67bb19f7c25a64f4d2c278b33e
NA980593 <90323608+NA980593@users.noreply.github.com>
2025-04-13 04:17:15 -0400

    auto model adding

--- cosmic-fishing/scripts/fish_spawner.gd
+++ cosmic-fishing/scripts/fish_spawner.gd
@@ -4,5 +4,6 @@ extends Node3D
 func spawn(name):
 	var scene = load("res://scenes/fish_pickable.tscn")
 	var instance = scene.instantiate()
+	instance.add_child(load("res://assets/3d_models/good-fish/"+name))
 	instance.global_transform = global_transform
 	add_child(instance)
